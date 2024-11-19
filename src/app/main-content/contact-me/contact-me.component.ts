fdb3fddd2a2de1b263ce83c3154b54eb71d4ac23
Christian-Kaden <christian.kaden.dev@gmail.com>
2024-11-19 22:07:43 +0100

    finished routing inside

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
+  aggrement = false;
+  contactForm!: FormGroup;
+  formSubmitted = false;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      privacy: [false, Validators.requiredTrue],
+    });
+  }
+
+  onSubmit(): void {
+    this.formSubmitted = true;
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+    }
+  }
+
+  isFieldValid(field: string): boolean {
+    const control = this.contactForm.get(field);
+    return control
+      ? control.valid &&
+          (control.dirty || control.touched || this.formSubmitted)
+      : false;
+  }
+  // ----------
+
+  acceptPrivacyPolicy() {
+    this.aggrement = !this.aggrement;
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isOpen = false;
+  menuState = '';
 
+  ngOnInit() {
+    this.checkWindowSize();
+  }
+
+  toggleMenu() {
+    if (this.isOpen) {
+      this.menuState = 'close';
+      setTimeout(() => {
+        this.isOpen = false;
+        this.restoreScroll();
+      }, 300);
+    } else {
+      this.isOpen = true;
+      this.menuState = 'open';
+      this.disableScroll();
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkWindowSize();
+  }
+
+  private checkWindowSize() {
+    if (window.innerWidth > 680) {
+      this.menuState = 'close';
+      this.isOpen = false;
+    }
+  }
+
+  private disableScroll() {
+    document.body.style.overflow = 'hidden'; // Deaktiviert Scrollen
+  }
+
+  private restoreScroll() {
+    document.body.style.overflow = ''; // Setzt Standardwert zurück
+  }
 }
